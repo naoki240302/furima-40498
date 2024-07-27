bfe58b9bacf28cc90b58a75855674200624dd64c
naoki240302 <bridgeover77@gmail.com>
2024-07-27 21:00:50 +0900

    Userモデルの単体テストコードの実装＋バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :kana_last, presence: true
-  validates :kana_first, presence: true
+  validates :last_name, presence: true,
+                        format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters' }
+  validates :kana_last, presence: true,
+                        format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width characters' }
+  validates :kana_first, presence: true,
+                         format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width characters' }
   validates :birthday, presence: true
 end

50493c6f5a5a7384f2132a2e6f097ef1674a56a8
naoki240302 <bridgeover77@gmail.com>
2024-08-03 17:03:46 +0900

    商品出品ページの実装＋商品内容の保存機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,24 @@
 class ItemsController < ApplicationController
-
   def index
   end
-  
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to '/'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :explanation, :price, :image, :category_id, :condition_id, :charge_load_id,
+                                 :shipping_time_id, :area_id).merge(user_id: current_user.id)
+  end
 end

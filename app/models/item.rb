4bd417f441f3a8c7f632de335a18456f5632070e
naoki240302 <bridgeover77@gmail.com>
2024-08-03 13:58:01 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
+  has_one_attached :image
+
+  validates :item_name, presence: true
+  validates :explanation, presence: true
+  validates :price, presence: true
+
+  validates :category_id, presence: true, numericality: { other_than: 1 }
+  validates :condition_id, presence: true, numericality: { other_than: 1 }
+  validates :charge_load_id, presence: true, numericality: { other_than: 1 }
+  validates :shipping_time_id, presence: true, numericality: { other_than: 1 }
+  validates :area_id, presence: true, numericality: { other_than: 1 }
 end
